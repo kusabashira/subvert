0be8b4414733369624b9cdcf66575bc256f632a0
wara <kusabashira227@gmail.com>
2015-06-12 11:40:54 +0900

    Add test for matcher

--- subvert_test.go
+++ subvert_test.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"reflect"
+	"regexp"
+	"testing"
+)
+
+var genMatcherTests = []struct {
+	src string
+	dst *regexp.Regexp
+}{
+	{"abc", regexp.MustCompile(`abc`)},
+}
+
+func TestGenMatcher(t *testing.T) {
+	for _, test := range genMatcherTests {
+		expect := test.dst
+		actual, err := newMatcher(test.src)
+		if err != nil {
+			t.Errorf("NewSubvert(%q) returns %q, want nil",
+				test.src, err)
+		}
+		if !reflect.DeepEqual(actual, expect) {
+			t.Errorf("%q: got %q, want %q",
+				test.src, actual, expect)
+		}
+	}
+}
