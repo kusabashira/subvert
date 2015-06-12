e3e2f012f88db933bdffdd165ea3a1b30e226684
wara <kusabashira227@gmail.com>
2015-06-12 17:06:30 +0900

    Implement sequense matcher as rough

--- subvert.go
+++ subvert.go
@@ -7,21 +7,28 @@ import (
 )
 
 var (
-	branches = regexp.MustCompile(`(?:[^,\\]|\\.)*`)
+	branches  = regexp.MustCompile(`(?:[^,\\]|\\.)*`)
+	sequenses = regexp.MustCompile(`(?:[^~\\]|\\.)*`)
 )
 
 func newMatcher(pat string) (m *regexp.Regexp, err error) {
 	pat = strings.Replace(pat, `\,`, `\\,`, -1)
+	pat = strings.Replace(pat, `\~`, `\\~`, -1)
 	pat = `"` + pat + `"`
 	pat, err = strconv.Unquote(pat)
 	if err != nil {
 		return nil, err
 	}
-	sp := branches.FindAllString(pat, -1)
-	for i := 0; i < len(sp); i++ {
-		sp[i] = strings.Replace(sp[i], `\,`, `,`, -1)
-		sp[i] = regexp.QuoteMeta(sp[i])
+	sls := sequenses.FindAllString(pat, -1)
+	for si := 0; si < len(sls); si++ {
+		bls := branches.FindAllString(sls[si], -1)
+		for bi := 0; bi < len(bls); bi++ {
+			bls[bi] = strings.Replace(bls[bi], `\,`, `,`, -1)
+			bls[bi] = strings.Replace(bls[bi], `\~`, `~`, -1)
+			bls[bi] = regexp.QuoteMeta(bls[bi])
+		}
+		sls[si] = "(" + strings.Join(bls, "|") + ")"
 	}
-	pat = "(" + strings.Join(sp, "|") + ")"
+	pat = strings.Join(sls, "")
 	return regexp.Compile(pat)
 }
