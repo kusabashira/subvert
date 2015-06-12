c71659b79f86235d9630d7805ef55f9f957df4a7
wara <kusabashira227@gmail.com>
2015-06-12 15:02:22 +0900

    Implement unquote special values in newMatcher

--- subvert.go
+++ subvert.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -9,7 +10,13 @@ var (
 	branches = regexp.MustCompile(`(?:[^,\\]|\\.)*`)
 )
 
-func newMatcher(pat string) (*regexp.Regexp, error) {
+func newMatcher(pat string) (m *regexp.Regexp, err error) {
+	pat = strings.Replace(pat, `\,`, `\\,`, -1)
+	pat = `"` + pat + `"`
+	pat, err = strconv.Unquote(pat)
+	if err != nil {
+		return nil, err
+	}
 	sp := branches.FindAllString(pat, -1)
 	for i := 0; i < len(sp); i++ {
 		sp[i] = strings.Replace(sp[i], `\,`, `,`, -1)
