4310dfb344443e720c26f521cf1edc3c8c868ce2
wara <kusabashira227@gmail.com>
2015-06-12 14:06:35 +0900

    Implement branch matcher

--- subvert.go
+++ subvert.go
@@ -5,8 +5,13 @@ import (
 	"strings"
 )
 
+var (
+	branches = regexp.MustCompile(`(?:[^,\\]|\\.)*`)
+)
+
 func newMatcher(pat string) (*regexp.Regexp, error) {
-	sp := strings.Split(pat, ",")
+	sp := branches.FindAllString(pat, -1)
 	pat2 := "(" + strings.Join(sp, "|") + ")"
-	return regexp.Compile(pat2)
+	pat3 := strings.Replace(pat2, `\,`, `,`, -1)
+	return regexp.Compile(pat3)
 }
