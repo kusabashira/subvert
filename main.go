88277b3dc10a537cba0099f324bbc6763d312406
wara <kusabashira227@gmail.com>
2015-06-13 20:33:21 +0900

    Implement usage

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"os"
+)
+
+func usage() {
+	os.Stderr.WriteString(`
+Usage: subvert [OPTION]... FROM TO [FILE]...
+Substitute multiple words at once.
+
+Options:
+  -h, --help                show this help message
+`[1:])
+}
