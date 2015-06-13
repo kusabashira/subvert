ea72c9509d214d28e6dda016e634ae4736b8cc04
wara <kusabashira227@gmail.com>
2015-06-13 09:24:31 +0900

    Extract parseExpr

--- subvert.go
+++ subvert.go
@@ -11,15 +11,15 @@ var (
 	branches  = regexp.MustCompile(`(?:[^,\\]|\\.)*`)
 )
 
-func newMatcher(expr string) (m *regexp.Regexp, err error) {
+func parseExpr(expr string) ([][]string, error) {
 	expr = strings.Replace(expr, `\,`, `\\,`, -1)
 	expr = strings.Replace(expr, `\/`, `\\/`, -1)
-	expr, err = strconv.Unquote(`"` + expr + `"`)
+	expr, err := strconv.Unquote(`"` + expr + `"`)
 	if err != nil {
 		return nil, err
 	}
-
 	sls := sequenses.FindAllString(expr, -1)
+	tree := make([][]string, len(sls))
 	for si := 0; si < len(sls); si++ {
 		bls := branches.FindAllString(sls[si], -1)
 		for bi := 0; bi < len(bls); bi++ {
@@ -27,6 +27,18 @@ func newMatcher(expr string) (m *regexp.Regexp, err error) {
 			bls[bi] = strings.Replace(bls[bi], `\/`, `/`, -1)
 			bls[bi] = regexp.QuoteMeta(bls[bi])
 		}
+		tree[si] = bls
+	}
+	return tree, nil
+}
+
+func newMatcher(expr string) (m *regexp.Regexp, err error) {
+	tree, err := parseExpr(expr)
+	if err != nil {
+		return nil, err
+	}
+	sls := make([]string, len(tree))
+	for si, bls := range tree {
 		sls[si] = "(" + strings.Join(bls, "|") + ")"
 	}
 	return regexp.Compile(strings.Join(sls, ""))
